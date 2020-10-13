47f6b1f30208ec464ae36ed4a2cc070cb445cc3c
DominikBielecki <bielecki.dominik@gmail.com>
2020-10-13 20:13:53 +0200

    Defining routes, configure Router based on routes, defining Router Outlet, set up Router Links to pass category id param, update product.service.ts to call new URL from Sping Boot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {ProductService} from './services/product.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
-// @ts-ignore
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+  /* this is generic wildcard, it will match anything that didn't match above routes*/
+];
+/* @ts-ignore */
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
