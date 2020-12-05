6778fc3b205b859a66ceb01c26cb2d7e6dd78dc4
DominikBielecki <bielecki.dominik@gmail.com>
2020-12-05 17:50:01 +0100

    Adding Pagination

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -30,10 +30,26 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  getProductListPaginate(thePage: number, thePageSize: number, theCategoryId: number): Observable<GetResponseProducts>{
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                        + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   searchProducts(theKeyword: string): Observable<Product[]>{
   const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
   return this.getProducts(searchUrl);
+  }
+
+  searchProductsPaginate(thePage: number, thePageSize: number, theKeyword: string): Observable<GetResponseProducts>{
+
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
 private getProducts(searchUrl: string): Observable<Product[]>{
@@ -53,6 +69,12 @@ private getProducts(searchUrl: string): Observable<Product[]>{
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   };
 }
 
