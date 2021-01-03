af55ae2a0ea0a2ee9067dac1e1b703905898260f
DominikBielecki <bielecki.dominik@gmail.com>
2021-01-03 17:30:15 +0100

    Added validation for checkout form.

---
+++ src/app/validators/aviation-pilot-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { AviationPilotShopValidators } from './aviation-pilot-shop-validators';
+
+describe('AviationPilotShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new AviationPilotShopValidators()).toBeTruthy();
+  });
+});
